0ea7c158664d3a8f4707ff2c87cc5a6cf29dc08f
Valerio Mariani <valerio.mariani@desy.de>
2017-07-16 23:29:09 -0700

    Updated setup scripts

--- setup-bash-petra11.sh
+++ setup-bash-petra11.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+export PATH=/home/p11user/CfelSoft/anaconda/bin:$PATH
 export ONDA_INSTALLATION_DIR=/home/p11user/CfelSoft/onda
 
-export LD_LIBRARY_PATH=${ONDA_CHEETAH_LIBRARY_DIR}:${ONDA_INSTALLATION_DIR}/python_extensions/:${LD_LIBRARY_PATH}
 export PYTHONPATH=${ONDA_INSTALLATION_DIR}:${PYTHONPATH}
 export PATH=${ONDA_INSTALLATION_DIR}:${ONDA_INSTALLATION_DIR}/GUI/:${ONDA_INSTALLATION_DIR}/tools/:${PATH}
