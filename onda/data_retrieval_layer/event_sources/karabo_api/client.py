d46ddd3385fe10467af5b66a634f60ca1ff3f175
Valerio Mariani <valerio.mariani@desy.de>
2018-08-22 18:26:18 +0200

    Updated Karabo API

--- onda/data_retrieval_layer/event_sources/karabo_api/client.py
+++ onda/data_retrieval_layer/event_sources/karabo_api/client.py
@@ -13,7 +13,6 @@ from functools import partial
 import msgpack
 import numpy as np
 import pickle
-import re
 import zmq
 
 
@@ -26,7 +25,7 @@ class Client:
     This class can request data to a Karabo bridge server.
     Create the client with::
 
-        from euxfel_karabo_bridge import Client
+        from karabo_bridge import Client
         krb_client = Client("tcp://153.0.55.21:12345")
 
     then call ``data = krb_client.next()`` to request next available data
@@ -37,7 +36,7 @@ class Client:
     endpoint : str
         server socket you want to connect to (only support TCP socket).
     sock : str, optional
-        socket type - supported: REQ.
+        socket type - supported: REQ, SUB.
     ser : str, optional
         Serialization protocol to use to decode the incoming message (default
         is msgpack) - supported: msgpack,pickle.
@@ -45,13 +44,11 @@ class Client:
     Raises
     ------
     NotImplementedError
-        if socker type or serialization algorythm is not supported.
-    SyntaxError
+        if socket type or serialization algorythm is not supported.
+    ZMQError
         if provided endpoint is not valid.
     """
     def __init__(self, endpoint, sock='REQ', ser='msgpack'):
-        if re.match(r'^tcp://.*:\d{1,5}$', endpoint) is None:
-            raise SyntaxError("Provided endpoint is invalid:", str(endpoint))
 
         self._context = zmq.Context()
         self._socket = None
@@ -82,6 +79,17 @@ class Client:
         """Request next data container.
 
         This function call is blocking.
+
+        Returns
+        -------
+        data : dict
+            The data for this train, keyed by source name.
+        meta : dict
+            The metadata for this train, keyed by source name.
+
+            This dictionary is populated for protocol version 1.0 and 2.2.
+            For other protocol versions, metadata information is available in
+            `data` dict.
         """
         if self._pattern == zmq.REQ:
             self._socket.send(b'next')
@@ -89,30 +97,48 @@ class Client:
         return self._deserialize(msg)
 
     def _deserialize(self, msg):
-        if len(msg) < 2:
-            return self._deserializer(msg[-1])
+        if len(msg) < 2:  # protocol version 1.0
+            data = self._deserializer(msg[-1])
+            meta = {}
+            for key, value in data.items():
+                meta[key] = value.get('metadata', {})
+            return data, meta
 
-        dat = {}
-        for header, data in zip(*[iter(msg)]*2):
+        data = {}
+        meta = {}
+        for header, payload in zip(*[iter(msg)]*2):
             md = self._deserializer(header)
             source = md['source']
             content = md['content']
 
             if content in ('msgpack', 'pickle.HIGHEST_PROTOCOL',
                            'pickle.DEFAULT_PROTOCOL'):
-                dat[source] = self._deserializer(data)
+                data[source] = self._deserializer(payload)
+                meta[source] = md.get('metadata', {})
             elif content in ('array', 'ImageData'):
                 dtype = md['dtype']
                 shape = md['shape']
 
-                buf = memoryview(data)
+                buf = memoryview(payload)
                 array = np.frombuffer(buf, dtype=dtype).reshape(shape)
 
                 if content == 'array':
-                    dat[source].update({md['path']: array})
+                    data[source].update({md['path']: array})
                 else:
-                    dat[source].update({md['path']: md['params']})
-                    dat[source][md['path']]['Data'] = array
+                    data[source].update({md['path']: md['params']})
+                    data[source][md['path']]['Data'] = array
             else:
                 raise RuntimeError('unknown message content:', md['content'])
-        return dat
+        return data, meta
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._context.destroy(linger=0)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
