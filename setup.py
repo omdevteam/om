e8dbba2080c70a6e506dfd3e0fce6ed0582c9093
Valerio Mariani <valerio.mariani@desy.de>
2018-09-28 13:31:49 -0700

    Fixed problem with cython extension not being built when using python 2

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with OnDA.  If not, see <http://www.gnu.org/licenses/>.
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function
 
 from setuptools import Extension, setup
 
