01f5fb808a564eb59470e723455bdbed5a503c30
Valerio Mariani <valerio.mariani@desy.de>
2018-11-13 18:31:57 +0100

    Removed copyright character

--- onda/utils/swaxs.py
+++ onda/utils/swaxs.py
@@ -13,7 +13,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with OnDA.  If not, see <http://www.gnu.org/licenses/>.
 #
-#    Copyright ©
+#    Copyright
 #
 """
 Processing of SWAXS data.
