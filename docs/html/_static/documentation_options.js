d8fa13612aa824f7a688d5a03df569f50aca2f83
Valerio Mariani <valmar@slac.stanford.edu>
2020-07-21 20:14:51 +0200

    Updated docs

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '20.08.0.0',
+    VERSION: '20.08.0.0dev',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
