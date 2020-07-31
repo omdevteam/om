eaebc6d638f72ac0ae7d75d36652c6c19468d28a
Valerio Mariani <valmar@slac.stanford.edu>
2020-07-31 12:28:39 +0200

    Added typing_extensions to requirements

--- setup.py
+++ setup.py
@@ -116,6 +116,7 @@ setup(
         "pyzmq>=18.0.2",
         "scipy>=1.2.1",
         "typing>=3.7.4.1",
+        "typing_extensions>=3.7.4.1",
     ],
     extras_require={":python_version < '3.4'": ["pathlib>=1.0.1"]},
     entry_points={"console_scripts": ["om_monitor.py=om.monitor:main"]},
