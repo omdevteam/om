f3342f6023d77b35e50920ff7f5a8c56a7ff451a
Valerio Mariani <valmar@slac.stanford.edu>
2021-06-11 18:17:02 -0700

    Added script to create a conda development environment for OM

---
+++ tools/scripts/conda_environment.sh
@@ -0,0 +1,2 @@
+conda create -n om-dev python click fabio h5py msgpack-python mypy_extensions numpy pyyaml pyzmq scipy typing_extensions pyqt pyqtgraph mkdocs mkdocstrings mkdocs-click mkdocs-material
+
