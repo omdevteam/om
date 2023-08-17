38585ea9fc0f461fb18e92db1e6a0c7629cfd4f1
Valerio Mariani <valmar@slac.stanford.edu>
2023-08-17 00:29:54 -0700

    Removed conda script from tools

--- tools/scripts/conda/conda_environment_dev.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-conda create -n om-dev python click fabio h5py msgpack-python mypy_extensions numpy pyyaml pyzmq rich scipy typing_extensions pyqt pyqtgraph mkdocs mkdocstrings mkdocs-click mkdocs-material
-
