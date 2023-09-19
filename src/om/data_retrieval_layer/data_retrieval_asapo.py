1c591f32062d9a35e5ae7669a6b590997209e89b
Aleksandra Tolstikova <alexandra.tolstikova@desy.de>
2023-09-19 12:00:11 +0200

    Added Pilatus data retrieval from ASAPO

--- src/om/data_retrieval_layer/data_retrieval_asapo.py
+++ src/om/data_retrieval_layer/data_retrieval_asapo.py
@@ -24,10 +24,11 @@ framework (used at the PETRA III facility).
 from typing import Dict
 
 from om.data_retrieval_layer.data_event_handlers_asapo import AsapoDataEventHandler
+from om.data_retrieval_layer.data_sources_generic import FloatEntryFromConfiguration
 from om.data_retrieval_layer.data_sources_asapo import (
     BeamEnergyAsapo,
     DetectorDistanceAsapo,
-    EigerAsapo,
+    DetectorDataAsapo,
     EventIdAsapo,
     TimestampAsapo,
 )
@@ -83,7 +84,7 @@ class EigerAsapoDataRetrieval(OmDataRetrievalProtocol):
             "event_id": EventIdAsapo(
                 data_source_name="eventid", monitor_parameters=monitor_parameters
             ),
-            "detector_data": EigerAsapo(
+            "detector_data": DetectorDataAsapo(
                 data_source_name="detector", monitor_parameters=monitor_parameters
             ),
             "beam_energy": BeamEnergyAsapo(
@@ -92,6 +93,88 @@ class EigerAsapoDataRetrieval(OmDataRetrievalProtocol):
             ),
             "detector_distance": DetectorDistanceAsapo(
                 data_source_name="detector_distance",
+                monitor_parameters=monitor_parameters,
+            ),
+        }
+
+        self._data_event_handler: OmDataEventHandlerProtocol = AsapoDataEventHandler(
+            source=source,
+            monitor_parameters=monitor_parameters,
+            data_sources=data_sources,
+        )
+
+    def get_data_event_handler(self) -> OmDataEventHandlerProtocol:
+        """
+        Retrieves the Data Event Handler used by the Data Retrieval class.
+
+        Please see the documentation of the base Protocol class for additional
+        information about this method.
+
+        Returns:
+
+            The Data Event Handler used by the Data Retrieval class.
+        """
+        return self._data_event_handler
+
+
+class PilatusAsapoDataRetrieval(OmDataRetrievalProtocol):
+    """
+    See documentation of the `__init__` function.
+    """
+
+    def __init__(self, *, monitor_parameters: MonitorParameters, source: str):
+        """
+        Data retrieval for Pilatus detector from ASAP::O at the PETRA III facility.
+
+        This class implements OM's Data Retrieval Layer for a Pilatus detector using
+        the ASAP::O software framework.
+
+        This class implements the interface described by its base Protocol class.
+        Please see the documentation of that class for additional information about
+        the interface.
+
+        * This class considers an individual data event as equivalent to the content of
+          an ASAP::O event, which stores data related to a single detector frame.
+
+        * The ASAP::O stream name and the ID of the ASAP::O event within the stream,
+          combined into a single string, are used as event identifier.
+
+        * ASAP::O provides timestamp information for each event.
+
+        * Since ASAP::O does not provide any detector distance or beam energy
+          information for Pilatus detector, their values are retrieved from OM's
+          configuration parameters (specifically, the `fallback_detector_distance_in_mm`
+          and `fallback_beam_energy_in_eV` entries in the `data_retrieval_layer`
+          parameter group).
+
+        * The source string required by this Data Retrieval class is either the ID of
+          the beamtime for which OM is being used (for online data retrieval) or the ID
+          of the beamtime and the name of the ASAP::O stream separated by a colon (for
+          offline data retrieval).
+
+        Arguments:
+
+            monitor_parameters: An object storing OM's configuration parameters.
+
+            source: A string describing the data event source.
+        """
+
+        data_sources: Dict[str, OmDataSourceProtocol] = {
+            "timestamp": TimestampAsapo(
+                data_source_name="timestamp", monitor_parameters=monitor_parameters
+            ),
+            "event_id": EventIdAsapo(
+                data_source_name="eventid", monitor_parameters=monitor_parameters
+            ),
+            "detector_data": DetectorDataAsapo(
+                data_source_name="detector", monitor_parameters=monitor_parameters
+            ),
+            "beam_energy": FloatEntryFromConfiguration(
+                data_source_name="fallback_beam_energy_in_eV",
+                monitor_parameters=monitor_parameters,
+            ),
+            "detector_distance": FloatEntryFromConfiguration(
+                data_source_name="fallback_detector_distance_in_mm",
                 monitor_parameters=monitor_parameters,
             ),
         }
