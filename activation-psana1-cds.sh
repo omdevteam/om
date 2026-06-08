215232f3f74261e2d8b10fe0450626c279555ff8
Valerio Mariani <valmar@slac.stanford.edu>
2026-06-08 13:54:17 -0700

    Added infrastrcture for pixi envs on CDS at SLAC

---
+++ activation-psana1-cds.sh
@@ -0,0 +1,3 @@
+export SIT_DATA=/cds/group/psdm/data
+export SIT_ROOT=/cds/group/psdm
+export SIT_PSDM_DATA=/cds/data/psdm
---
+++ activation-psana2-cds.sh
@@ -0,0 +1,2 @@
+export DIR_PSDM=/cds/group/psdm
+export SIT_PSDM_DATA=/cds/data/psdm
